2479b88e4e1cc3158b98fce213e821a9ff542645
João Soares <jmkoygg@gmail.com>
2025-05-27 21:02:58 -0300

    Criando temas

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -5,7 +5,7 @@ const LoginArea = styled.div`
 `
 
 const LoginImage = styled.div`
-  background-image: url('/LoginImage.jpg');
+  background-image: url('/Login-Image.jpg');
   background-size: cover;
   height: 100vh;
   width: 50%;
