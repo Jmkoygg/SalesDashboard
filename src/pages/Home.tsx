114ca4d6b17c4bfc4acab2253c39fb686e526cc3
João Soares <jmkoygg@gmail.com>
2025-05-29 18:39:52 -0300

    Adicionando card

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,12 @@
-import Header from '@/components/Header'
+import { CardComponent, Header } from '@/components'
+import { Container } from '@mui/material'
 function Home() {
   return (
     <>
       <Header />
-      <h1>Home</h1>
+      <Container maxWidth="lg">
+        <CardComponent>Card</CardComponent>
+      </Container>
     </>
   )
 }
