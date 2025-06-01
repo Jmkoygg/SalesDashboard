bc75812e6545a12b92d7cea1001837c3a326c684
João Soares <jmkoygg@gmail.com>
2025-06-01 16:05:50 -0300

    logica de troca de tema

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,10 +1,33 @@
-import Header from '@/components/Header'
+import { CardComponent, Header, StyledH2, StyledButton } from '@/components'
+import { useContext } from 'react'
+import { AppThemeContext } from '@/contexts/AppThemeContext'
+import { Container, Grid } from '@mui/material'
 
-function Profile() {
+const Profile = () => {
+  const themeContext = useContext(AppThemeContext)
   return (
     <>
       <Header />
-      <h1>Profile</h1>
+      <Container className="mb-2" maxWidth="lg">
+        <Grid container spacing={4}>
+          <Grid item xs={12} sm={6}>
+            <CardComponent>Seus Dados...</CardComponent>
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <CardComponent>
+              <StyledH2 className="mb-1">Definições de cookies</StyledH2>
+              <StyledButton
+                className="primary mb-1"
+                onClick={themeContext?.toggleTheme}
+              >
+                Trocar para tema{' '}
+                {themeContext?.appTheme === 'light' ? 'escuro' : 'claro'}
+              </StyledButton>
+              <StyledButton className="alert">Logout</StyledButton>
+            </CardComponent>
+          </Grid>
+        </Grid>
+      </Container>
     </>
   )
 }
